d31dc360b56a869ac5af5e4b5d7cf9c1feb77d32
Asus <Asus@FZHNSA>
2022-11-28 16:27:50 +0700

    Add project files.

---
+++ StackUsingSingleLinkList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        { 
+        }
+    }
+    
+    
+    
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
