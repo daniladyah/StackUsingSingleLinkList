10dcd18c398c8a3f1e1f917156cfff986ea1a934
daniladyah <daniladyah68@gmail.com>
2022-11-28 16:54:35 +0700

    menambahkan case 1

--- StackUsingSingleLinkList/Program.cs
+++ StackUsingSingleLinkList/Program.cs
@@ -48,17 +48,46 @@ namespace StackUsingSingleLinkList
             Console.WriteLine("\n The poped elements is: " + top.info);
             top = top.next; // makw top point to the next node is squence
         }
-        
+        public void display()
+        {
+            node tmp;
+            if (empty())
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                for(tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+            }
+
+        }
     }
-    
-    
-    
-    
-    
     internal class Program
     {
         static void Main(string[] args)
         {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push ");
+                Console.WriteLine("2. Pop ");
+                Console.WriteLine("3. Display ");
+                Console.WriteLine("4. Exit ");
+                Console.WriteLine("\nEnter your choice: ");
+                string Input = Console.ReadLine();
+                char ch = Convert.ToChar(Input == "" ? "0" : Input);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number :");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                }
+            }
         }
     }
 }
